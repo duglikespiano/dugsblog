b3f3126d051c26abaf42aa91d3503ee47d249b9a
SUHO PARK <duglikespiano@gmail.com>
2024-02-03 03:14:15 +0900

    ETC : SETTINGS FOR READING MARKDOWN FILES ADDED

--- dist/router/mainRouter.js
+++ dist/router/mainRouter.js
@@ -1,14 +1,28 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const promises_1 = __importDefault(require("fs/promises"));
 const pingRouter_1 = __importDefault(require("./pingRouter"));
 const router = (0, express_1.Router)();
 router.use('/ping', pingRouter_1.default);
-router.get('/', (req, res) => {
-    res.render('main.ejs');
-});
+router.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const data = yield promises_1.default.readFile('./public/markdown/dummy.md', 'utf8');
+        res.render('main.ejs');
+    }
+    catch (error) { }
+}));
 exports.default = router;
 //# sourceMappingURL=mainRouter.js.map
--- src/router/mainRouter.ts
+++ src/router/mainRouter.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import fs from 'fs/promises';
 import pingRouter from './pingRouter';
 const router = Router();
 
 router.use('/ping', pingRouter);
-router.get('/', (req, res) => {
-	res.render('main.ejs');
+router.get('/', async (req, res) => {
+	try {
+		const data = await fs.readFile('./public/markdown/dummy.md', 'utf8');
+		res.render('main.ejs');
+	} catch (error) {}
 });
 
 export default router;
