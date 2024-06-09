1eb2b1f27ee09ecded3da343736e73697926c8cd
SUHO PARK <duglikespiano@gmail.com>
2024-06-09 17:42:35 +0900

    ETC : SERVER RELOAD SETTING ADDED

--- dist/app.js
+++ dist/app.js
@@ -7,13 +7,24 @@ exports.app = void 0;
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const livereload_1 = __importDefault(require("livereload"));
+const connect_livereload_1 = __importDefault(require("connect-livereload"));
 const rootRouter_1 = __importDefault(require("./router/rootRouter"));
 exports.app = (0, express_1.default)();
+const liveServer = livereload_1.default.createServer({
+    exts: ['ejs', 'scss', 'ts', 'css', 'js'],
+    debug: true,
+});
+liveServer.watch(path_1.default.join(__dirname, '../public'));
+liveServer.server.once('connection', () => {
+    liveServer.refresh('/');
+});
 exports.app.set('view engine', 'ejs');
 exports.app.set('views', path_1.default.join(__dirname, '../public/views'));
 exports.app.use(express_1.default.json());
 exports.app.use(express_1.default.urlencoded({ extended: true }));
 exports.app.use(express_1.default.static(path_1.default.join(__dirname, '../public')));
 exports.app.use((0, cookie_parser_1.default)());
+exports.app.use((0, connect_livereload_1.default)());
 exports.app.use(rootRouter_1.default);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,26 @@
 import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
+import livereload from 'livereload';
+import livereloadMiddleware from 'connect-livereload';
 import router from './router/rootRouter';
 
 export const app = express();
+const liveServer = livereload.createServer({
+	exts: ['ejs', 'scss', 'ts', 'css', 'js'],
+	debug: true,
+});
+liveServer.watch(path.join(__dirname, '../public'));
+liveServer.server.once('connection', () => {
+	liveServer.refresh('/');
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../public/views'));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(cookieParser());
+app.use(livereloadMiddleware());
 app.use(router);
